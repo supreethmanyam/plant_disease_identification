9253dcaa230b833457636eabe5ab9125d568ee43
supreethmanyam <supreethmanyam@gmail.com>
2019-05-12 18:57:03 +0530

    Added probability

--- app/server.py
+++ app/server.py
@@ -92,8 +92,8 @@ async def analyze(request):
     data = await request.form()
     img_bytes = await (data['file'].read())
     img = open_image(BytesIO(img_bytes))
-    prediction = learn.predict(img)[0]
-    return JSONResponse({'result': str(prediction)})
+    prediction, prediction_index, probabilities = learn.predict(img)
+    return JSONResponse({'result': str(prediction), 'probability': str(round(probabilities[prediction_index].item(), 2))})
 
 if __name__ == '__main__':
     if 'serve' in sys.argv: uvicorn.run(app=app, host='0.0.0.0', port=5042)
